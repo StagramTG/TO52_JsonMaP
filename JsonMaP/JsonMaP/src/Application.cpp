51135da771a0fc92a34cc9206aa9d8fdc4d916ee
Thomas Gredin <t.gredin@gmx.fr>
2018-11-08 13:23:06 +0100

    Create vs solution

---
+++ JsonMaP/JsonMaP/src/Application.cpp
@@ -0,0 +1,14 @@
+/*
+	Author: Thomas Gredin
+	Date: 7th November 2018
+
+	JsonMaP (Json Multiagents Process) is an application created for TO52 project.
+	The main goal is to simulate interactions and actions of series scipts' characters
+	in order to determine physical behavior in space that explain these interactions
+	between each characters of the given episode.
+*/
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
