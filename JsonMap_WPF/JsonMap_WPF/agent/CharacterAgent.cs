f3b43307c020533a26824f8f023ca0771008add1
Thomas Gredin <gredintom@gmail.com>
2018-11-10 22:43:12 +0100

    Create CharacterAgent class

---
+++ JsonMap_WPF/JsonMap_WPF/agent/CharacterAgent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace JsonMap.Agent
+{
+    public class CharacterAgent
+    {
+        public String Name { get; private set; }
+        public int Id { get; private set; }
+        public int Weight { get; private set; }
+
+        public CharacterAgent(String name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
