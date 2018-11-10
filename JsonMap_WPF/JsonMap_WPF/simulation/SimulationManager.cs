67ba635a388b03fc016e4ebafc6dbc992dd21aea
Thomas Gredin <gredintom@gmail.com>
2018-11-10 16:22:15 +0100

    Add Workers class file

---
+++ JsonMap_WPF/JsonMap_WPF/simulation/SimulationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JsonMap.Data;
+
+namespace JsonMap.Simulation
+{
+    public static class SimulationManager
+    {
+        /** Episode related */
+        public static Episode CurrentEpisode { get; set; }
+
+        /** Data to send through socket */
+        public static Object Locker = new Object();
+        public static String DataToSend { get; set; }
+        public static bool NewDataExists { get; set; } = false;
+
+        /** Simulation global stuffs */
+        public static bool SimulationShouldRun { get; set; } = false;
+        public static bool SimulationShouldPause { get; set; } = false;
+    }
+}
---
+++ JsonMap_WPF/JsonMap_WPF/simulation/Workers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonMap_WPF.simulation
+{
+    public static class Workers
+    {
+        /**
+         * Method that runs in simulation Thread.
+         * Runs multiagents system to calculate physical behaviors
+         * based on interactions extract from episode's script.
+         */
+        public static void SimulationWorker()
+        {
+
+        }
+
+        /**
+         * Method that runs in communication Thread.
+         * Send calculated data to render application through Socket.
+         */
+        public static void CommunicationWorker()
+        {
+
+        }
+    }
+}
