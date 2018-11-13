31d47f9c45de7b4c087db4b00fa0140e3557e834
Thomas Gredin <t.gredin@gmx.fr>
2018-11-13 18:11:36 +0100

    Add time manager class

--- JsonMap_WPF/JsonMap_WPF/math/RigidBody.cs
+++ JsonMap_WPF/JsonMap_WPF/math/RigidBody.cs
@@ -3,7 +3,58 @@ using System.Collections.Generic;
 
 namespace JsonMap.Math
 {
-    class RigidBody
+    public class RigidBody
     {
+        /**
+         * ==========================================
+         *      FIELDS
+         * ==========================================
+         */
+        private Vector3 position;
+        private Vector3 speed;
+        private Vector3 acceleration;
+
+        /**
+         * ==========================================
+         *      PROPERTIES
+         * ==========================================
+         */
+        public Vector3 Position        => position;
+        public Vector3 Speed           => speed;
+        public Vector3 Acceleration    => acceleration;
+
+        public bool UseGravity { get; set; } = true;
+
+        public RigidBody()
+        {
+            position = new Vector3(0, 0, 0);
+            speed = new Vector3(0, 0, 0);
+            acceleration = new Vector3(0, 0, 0);
+        }
+
+        public RigidBody(Vector3 pposition)
+        {
+            position = pposition;
+            speed = new Vector3(0, 0, 0);
+            acceleration = new Vector3(0, 0, 0);
+        }
+
+        public void ApplyForce(Vector3 pforce)
+        {
+
+        }
+
+        public void Update()
+        {
+            // Check if gravity influence the Rigid Body
+            if(UseGravity)
+            {
+                // Apply Gravity
+            }
+
+            // Apply acceleration to speed
+
+            // Apply speed to position
+        }
     }
 }
