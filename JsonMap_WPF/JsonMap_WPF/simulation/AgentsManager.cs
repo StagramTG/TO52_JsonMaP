f5b48eb6f1b8d7a8fe4d9c59c2d8e4efd0daaadc
Thomas Gredin <t.gredin@gmx.fr>
2018-11-14 22:49:15 +0100

    Begin to fill AgentsManager class

--- JsonMap_WPF/JsonMap_WPF/simulation/AgentsManager.cs
+++ JsonMap_WPF/JsonMap_WPF/simulation/AgentsManager.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 
 using JsonMap.Data;
+using JsonMap.Agent;
 
 namespace JsonMap.Simulation
 {
     public class AgentsManager
     {
+        Dictionary<string, CharacterAgent> agents;
+
         public AgentsManager()
         {
-
+            agents = new Dictionary<string, CharacterAgent>();
         }
 
         public bool Setup(Episode pepisode)
         {
+            // Create an agent for each characters for given episode
+            
+
             return true;
         }
     }
