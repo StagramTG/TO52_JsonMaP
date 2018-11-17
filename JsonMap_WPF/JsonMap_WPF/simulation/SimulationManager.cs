5e0bf06afbff218aea32a431e09962865c5f5d4d
Thomas Gredin <t.gredin@gmx.fr>
2018-11-17 13:33:30 +0100

    Delete unused stuff for communication

--- JsonMap_WPF/JsonMap_WPF/simulation/SimulationManager.cs
+++ JsonMap_WPF/JsonMap_WPF/simulation/SimulationManager.cs
@@ -11,11 +11,6 @@ namespace JsonMap.Simulation
         /** Episode related */
         public static Episode CurrentEpisode           { get; set; }
 
-        /** Data to send through socket */
-        public static Object Locker = new Object();
-        public static String DataToSend                { get; set; }
-        public static bool NewDataExists               { get; set; } = false;
-
         /** Simulation global stuffs */
         public static bool SimulationShouldRun         { get; set; } = false;
         public static bool SimulationShouldPause       { get; set; } = false;
