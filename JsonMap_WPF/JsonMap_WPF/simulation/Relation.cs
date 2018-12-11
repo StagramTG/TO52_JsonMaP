705b0085bd7674aeddb47521d295256718ea2da9
Thomas Gredin <t.gredin@gmx.fr>
2018-12-11 16:16:28 +0100

    Create Relation class

---
+++ JsonMap_WPF/JsonMap_WPF/simulation/Relation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JsonMap.Agent;
+
+namespace JsonMap.simulation
+{
+    public class Relation
+    {
+        private Tuple<CharacterAgent, CharacterAgent> involvedAgents;
+        private float strengh;
+        private float nature;
+
+        public Relation(CharacterAgent pAgent1, CharacterAgent pAgent2)
+        {
+            involvedAgents = new Tuple<CharacterAgent, CharacterAgent>(pAgent1, pAgent2);
+            strengh = 0f;
+            nature = 0f;
+        }
+    }
+}
